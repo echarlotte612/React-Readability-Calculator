1b7e2edef5af589659cd51b926ad7b8474128fd2
ecbaldocke612 <e.charlotte94@gmail.com>
2021-09-18 18:56:03 +0100

    added examples shuffle button

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { examples } from '../data/examples';
+import { FaRandom } from 'react-icons/fa';
 
 export default function Examples() {
+    const [index, setIndex] = useState(0);
+    const getRandom = () => {
+        // Gets a random example each time. //
+        const randomNumber = Math.floor(Math.random() * examples.length);
+        // If the random number is the same as previous, just call the function again until it's different //
+        if(randomNumber !== index) 
+        {
+            setIndex(randomNumber);
+        }
+        else
+        {
+            getRandom()
+        }
+    }
 
     return (
         <div>
-            {examples.map((eg) => (
-                <p>{eg.text}</p>
-            ))
-            }
+            <button onClick={() => getRandom()}><FaRandom /></button>
+            <p className="example">{examples[index].text}</p>
         </div>
     )
 }
